0b607f3964b1d0d9186f6c7d226733aade90b3c4
Gleb Myznikov <myznikovgleb@yandex.ru>
2023-12-19 21:49:40 +0300

    Update imports

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
-import { store } from './store/store'
-import { router } from './router'
+import { store } from '@/store/store'
+import { router } from '@/router'
 
 import './index.css'
 
