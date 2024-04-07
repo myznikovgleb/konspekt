9ff2d381f1b8a4f51c6c1c3b03c936660ae12994
Gleb Myznikov <myznikovgleb@yandex.ru>
2024-04-07 23:28:35 +0300

    Customize default themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   plugins: [require('daisyui')],
   daisyui: {
-    themes: ['light', 'dark'],
+    themes: [
+      {
+        light: {
+          ...require('daisyui/src/theming/themes')['light'],
+          primary: '#fb923c',
+        },
+      },
+      {
+        dark: {
+          ...require('daisyui/src/theming/themes')['dark'],
+          primary: '#fb923c',
+        },
+      },
+    ],
     logs: false,
   },
 }
