af177610f8bb3daba0c9a915489a73e629e4d037
Gleb Myznikov <myznikovgleb@yandex.ru>
2024-08-08 23:14:21 +0300

    Fix DnD for mobile devices

--- src/entities/file/ui/file-folder/file-folder.tsx
+++ src/entities/file/ui/file-folder/file-folder.tsx
@@ -1,138 +1,32 @@
-import { animated, config, useSprings } from '@react-spring/web'
-import { useDrag } from '@use-gesture/react'
-import { clamp } from 'lodash'
 import { useMemo } from 'react'
 
-import { useStoreDispatch } from '@/shared/lib'
-
-import { fileSlice, SIZE_ICON, ROW_NUMBER, COL_NUMBER } from '../../model'
 import { FileIcon } from '../file-icon'
 
-import type { File, Position } from '@/shared/api'
+import type { File } from '@/shared/api'
 
 interface FileFolderProps {
   files: File[]
 }
 
-const configurationFn = (
-  order: Array<Position>,
-  active = false,
-  indexDragged = 0,
-  x = 0,
-  y = 0
-) => {
-  return (index: number) => {
-    const isActiveDragged = active && index === indexDragged
-
-    const { row, col } = order[index]
-
-    return isActiveDragged
-      ? {
-          x: clamp(col * SIZE_ICON + x, 0, (COL_NUMBER - 1) * SIZE_ICON),
-          y: clamp(row * SIZE_ICON + y, 0, (ROW_NUMBER - 1) * SIZE_ICON),
-          scale: 1.1,
-          zIndex: 10,
-          immediate: (key: string) => key === 'zIndex',
-          config: (key: string) =>
-            key === 'x' || key === 'y' ? config.stiff : config.default,
-        }
-      : {
-          x: col * SIZE_ICON,
-          y: row * SIZE_ICON,
-          scale: 1,
-          zIndex: 0,
-          immediate: false,
-        }
-  }
-}
-
 const FileFolder = (props: FileFolderProps) => {
   const { files } = props
 
-  const dispatch = useStoreDispatch()
-
-  const positions = useMemo(
+  const occupiedPositions = useMemo(
     () => files.map(({ position }) => position),
     [files]
   )
 
-  const [springs, api] = useSprings(files.length, configurationFn(positions), [
-    files,
-  ])
-
-  const bind = useDrag(
-    ({ args, active, movement: [x, y], tap }) => {
-      if (tap) {
-        return
-      }
-
-      const [index] = args as [number]
-
-      if (!active) {
-        const {
-          id,
-          position: { row, col },
-        } = files[index]
-
-        const positionNext: Position = {
-          row: clamp(
-            Math.round((row * SIZE_ICON + y) / SIZE_ICON),
-            0,
-            ROW_NUMBER - 1
-          ),
-          col: clamp(
-            Math.round((col * SIZE_ICON + x) / SIZE_ICON),
-            0,
-            COL_NUMBER - 1
-          ),
-        }
-
-        const isUpdateRequired = !positions.some(
-          (position) =>
-            position.row === positionNext.row &&
-            position.col === positionNext.col
-        )
-
-        if (isUpdateRequired) {
-          const positionsNext = positions.slice()
-          positionsNext[index] = positionNext
-
-          api.start(configurationFn(positionsNext, false, index, x, y))
-
-          dispatch(fileSlice.actions.updateOne({ id, position: positionNext }))
-
-          return
-        }
-      }
-
-      api.start(configurationFn(positions, active, index, x, y))
-    },
-    { filterTaps: true }
-  )
-
-  const onAfterRemove = (indexRemoved: number) => {
-    for (let i = indexRemoved; i < api.current.length - 1; i++) {
-      api.current[i].set(api.current[i + 1].get())
-    }
-  }
-
   return (
     <ul className="relative size-full p-5">
-      {files.map((file, index) => (
-        <animated.li
+      {files.map((file) => (
+        <FileIcon
           key={file.id}
-          {...bind(index)}
-          className="absolute touch-none"
-          style={springs[index]}
-        >
-          <FileIcon
-            filename={file.filename}
-            id={file.id}
-            permission={file.permission}
-            index={index}
-            onAfterRemove={onAfterRemove}
-          />
-        </animated.li>
+          filename={file.filename}
+          id={file.id}
+          permission={file.permission}
+          position={file.position}
+          occupiedPositions={occupiedPositions}
+        />
       ))}
     </ul>
   )
--- src/entities/file/ui/file-icon/file-icon.tsx
+++ src/entities/file/ui/file-icon/file-icon.tsx
@@ -1,56 +1,158 @@
 import { BookOpenIcon } from '@heroicons/react/24/solid'
 import * as Popover from '@radix-ui/react-popover'
+import { animated, config, useSpring } from '@react-spring/web'
+import { useGesture } from '@use-gesture/react'
+import { clamp } from 'lodash'
 import { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { useLongPress } from 'react-use'
+import { match } from 'ts-pattern'
 
 import { Permission } from '@/shared/api'
 import { useStoreDispatch } from '@/shared/lib'
 
-import { fileSlice } from '../../model'
+import { fileSlice, SIZE_ICON, ROW_NUMBER, COL_NUMBER } from '../../model'
 import { FileContext } from '../file-context'
 
-import type { File } from '@/shared/api'
+import type { File, Position } from '@/shared/api'
 import type { MouseEvent, PointerEvent } from 'react'
 
-interface FileIconProps extends Pick<File, 'filename' | 'id' | 'permission'> {
-  index: number
-  onAfterRemove: (indexRemoved: number) => void
+interface FileIconProps
+  extends Pick<File, 'filename' | 'id' | 'permission' | 'position'> {
+  occupiedPositions: Array<Position>
+}
+
+enum IconState {
+  Idle,
+  Opened,
+  Dragged,
+}
+
+const LONG_PRESS_DELAY_MS = 500
+
+const configurationFn = (
+  { row, col }: Position,
+  active = false,
+  x = 0,
+  y = 0
+) => {
+  return () => {
+    return active
+      ? {
+          x: clamp(col * SIZE_ICON + x, 0, (COL_NUMBER - 1) * SIZE_ICON),
+          y: clamp(row * SIZE_ICON + y, 0, (ROW_NUMBER - 1) * SIZE_ICON),
+          scale: 1.1,
+          zIndex: 10,
+          immediate: (key: string) => key === 'zIndex',
+          config: (key: string) =>
+            key === 'x' || key === 'y' ? config.stiff : config.default,
+        }
+      : {
+          x: col * SIZE_ICON,
+          y: row * SIZE_ICON,
+          scale: 1,
+          zIndex: 0,
+          immediate: false,
+        }
+  }
 }
 
 const FileIcon = (props: FileIconProps) => {
-  const { filename, id, permission, index, onAfterRemove } = props
+  const { filename, id, permission, position, occupiedPositions } = props
+  const { row, col } = position
 
   const dispatch = useStoreDispatch()
   const navigate = useNavigate()
 
-  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [iconState, setIconState] = useState<IconState>(IconState.Idle)
 
   const refTrigger = useRef<HTMLButtonElement>(null)
+  const refTimer = useRef<ReturnType<typeof setTimeout>>()
 
-  const longPressEvent = useLongPress(
-    () => {
-      refTrigger.current?.blur()
+  const [spring, api] = useSpring(configurationFn(position), [position])
 
-      setIsOpen(true)
-    },
+  const bind = useGesture(
     {
-      isPreventDefault: false,
-      delay: 500,
-    }
+      onDrag: ({ active, movement: [x, y], tap }) => {
+        if (tap) {
+          return
+        }
+
+        match(iconState)
+          .with(IconState.Dragged, () => {})
+          .otherwise(() => {
+            clearTimeout(refTimer.current)
+
+            setIconState(IconState.Dragged)
+          })
+
+        if (!active) {
+          const positionNext: Position = {
+            row: clamp(
+              Math.round((row * SIZE_ICON + y) / SIZE_ICON),
+              0,
+              ROW_NUMBER - 1
+            ),
+            col: clamp(
+              Math.round((col * SIZE_ICON + x) / SIZE_ICON),
+              0,
+              COL_NUMBER - 1
+            ),
+          }
+
+          const isUpdateRequired = !occupiedPositions.some(
+            (occupiedPosition) =>
+              occupiedPosition.row === positionNext.row &&
+              occupiedPosition.col === positionNext.col
+          )
+
+          if (isUpdateRequired) {
+            api.start(configurationFn(positionNext, false, x, y))
+
+            dispatch(
+              fileSlice.actions.updateOne({ id, position: positionNext })
+            )
+
+            return
+          }
+        }
+
+        api.start(configurationFn(position, active, x, y))
+      },
+      onPointerDown: () => {
+        refTrigger.current?.focus()
+
+        refTimer.current = setTimeout(() => {
+          setIconState(IconState.Opened)
+        }, LONG_PRESS_DELAY_MS)
+      },
+      onPointerUp: () => {
+        clearTimeout(refTimer.current)
+
+        match(iconState)
+          .with(IconState.Dragged, () => {
+            setIconState(IconState.Idle)
+          })
+          .otherwise(() => {})
+      },
+    },
+    { drag: { filterTaps: true } }
   )
 
   const onContextMenu = (event: MouseEvent) => {
     event.preventDefault()
 
-    setIsOpen(true)
+    setIconState(IconState.Opened)
   }
 
   const onClick = (event: MouseEvent) => {
     const pointerEvent = event as PointerEvent
     const pointerType = pointerEvent.nativeEvent.pointerType
 
-    if (pointerType === 'mouse' || pointerType === undefined) {
+    if (
+      pointerType === 'mouse' ||
+      pointerType === 'touch' ||
+      pointerType === undefined
+    ) {
       event.preventDefault()
     }
 
@@ -67,45 +169,44 @@ const FileIcon = (props: FileIconProps) => {
 
   const onRemove = () => {
     dispatch(fileSlice.actions.removeOne({ id }))
-
-    onAfterRemove(index)
   }
 
+  const onOpenChange = (open: boolean) => {
+    setIconState(open ? IconState.Opened : IconState.Idle)
+  }
+
+  const open = iconState === IconState.Opened
   const isWriteForbidden = permission !== Permission.Write
 
   return (
-    <Popover.Root open={isOpen} onOpenChange={setIsOpen}>
-      <Popover.Trigger asChild>
-        <button
-          ref={refTrigger}
-          onTouchStart={(event) => {
-            longPressEvent.onTouchStart(event)
-          }}
-          onTouchEnd={() => {
-            longPressEvent.onTouchEnd()
-          }}
-          onClick={onClick}
-          onDoubleClick={onOpen}
-          onContextMenu={onContextMenu}
-          className="flex size-28 cursor-pointer select-none flex-col items-center gap-2 rounded-xl p-2 hover:bg-base-200"
-        >
-          <div className="rounded-xl bg-base-300 p-4">
-            <BookOpenIcon className="size-8" />
-          </div>
-          <div className="w-full truncate text-center text-base font-semibold">
-            {filename}
-          </div>
-        </button>
-      </Popover.Trigger>
-      <Popover.Portal>
-        <Popover.Content>
-          <FileContext
-            onOpen={onOpen}
-            onRemove={isWriteForbidden ? undefined : onRemove}
-          />
-        </Popover.Content>
-      </Popover.Portal>
-    </Popover.Root>
+    <animated.li {...bind()} className="absolute touch-none" style={spring}>
+      <Popover.Root open={open} onOpenChange={onOpenChange}>
+        <Popover.Trigger asChild>
+          <button
+            ref={refTrigger}
+            onClick={onClick}
+            onDoubleClick={onOpen}
+            onContextMenu={onContextMenu}
+            className="flex size-28 cursor-pointer select-none flex-col items-center gap-2 rounded-xl p-2 md:hover:bg-base-200"
+          >
+            <div className="rounded-xl bg-base-300 p-4">
+              <BookOpenIcon className="size-8" />
+            </div>
+            <div className="w-full truncate text-center text-base font-semibold">
+              {filename}
+            </div>
+          </button>
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content>
+            <FileContext
+              onOpen={onOpen}
+              onRemove={isWriteForbidden ? undefined : onRemove}
+            />
+          </Popover.Content>
+        </Popover.Portal>
+      </Popover.Root>
+    </animated.li>
   )
 }
 
