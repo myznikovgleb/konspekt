c460f9503ef902df9d239917775f6455f349128d
Gleb Myznikov <myznikovgleb@yandex.ru>
2025-06-12 20:24:32 +0300

    Upgrade Tailwind CSS integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { fileURLToPath, URL } from 'url'
 
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     host: true,
   },
